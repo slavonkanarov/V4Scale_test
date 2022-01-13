bfdda523c1f22dbba276cf0ae289bcc23eb8562c
Вячеслав <v.kanarov>
2022-01-13 12:46:28 +0300

    added fixsture

--- test_BD.py
+++ test_BD.py
@@ -2,136 +2,78 @@ import BD
 import pytest
 import sqlite3
 
-path = "test_BD"
-
-
-def test_init():
-    global path
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 0
-    con.close()
-
-    bd = BD.BD(path)
-    del bd
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    con.close()
-
-
-def test_add_value():
-    global path
-
+@pytest.fixture()
+def initBDpath(tmpdir):
+    tmpdir.join("test_BD")
+    path = str(tmpdir)+"test_BD"
     con = sqlite3.connect(path)
     cur = con.cursor()
     tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
     cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
     con.close()
+    yield path
 
-    bd = BD.BD(path)
-    bd.add_value({'memory': 62.5, 'cpu': 10.25, 'disk': 23.2})
-    del bd
-
+def checkBDhasTable(path):
     con = sqlite3.connect(path)
     cur = con.cursor()
     assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
     d = cur.execute("SELECT * FROM data;").fetchall()
+    con.close()
+    return d
+
+def test_init(initBDpath):
+    bd = BD.BD(initBDpath)
+    del bd
+    checkBDhasTable(initBDpath)
+
+
+def test_add_value(initBDpath):
+    bd = BD.BD(initBDpath)
+    bd.add_value({'memory': 62.5, 'cpu': 10.25, 'disk': 23.2})
+    del bd
+
+    d = checkBDhasTable(initBDpath)
     assert len(d) == 1
     assert d[0][0] == 62.5
     assert d[0][1] == 10.25
     assert d[0][2] == 23.2
-    con.close()
 
 
-def test_add_value_wrong_value_1():
-    global path
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    con.close()
-
-    bd = BD.BD(path)
+def test_add_value_wrong_value_1(initBDpath):
+    bd = BD.BD(initBDpath)
     with pytest.raises(ValueError):
         bd.add_value({'memry': 62.5, 'cpu': 10.25, 'disk': 23.2})
     del bd
 
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    d = cur.execute("SELECT * FROM data;").fetchall()
+    d = checkBDhasTable(initBDpath)
     assert len(d) == 0
-    con.close()
 
 
-def test_add_value_wrong_value_2():
-    global path
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    con.close()
-
-    bd = BD.BD(path)
+def test_add_value_wrong_value_2(initBDpath):
+    bd = BD.BD(initBDpath)
     with pytest.raises(ValueError):
         bd.add_value({'cpu': 10.25, 'disk': 23.2})
     del bd
 
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    d = cur.execute("SELECT * FROM data;").fetchall()
+    d = checkBDhasTable(initBDpath)
     assert len(d) == 0
-    con.close()
 
 
-def test_add_value_wrong_value_3():
-    global path
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    con.close()
-
-    bd = BD.BD(path)
+def test_add_value_wrong_value_3(initBDpath):
+    bd = BD.BD(initBDpath)
     with pytest.raises(ValueError):
         bd.add_value({'memory': 62.5, 'cpu|disk': 10.25, 'disk': 23.2})
     del bd
 
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    d = cur.execute("SELECT * FROM data;").fetchall()
+    d = checkBDhasTable(initBDpath)
     assert len(d) == 0
-    con.close()
 
 
-def test_add_value_wrong_value_4():
-    global path
-
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    con.close()
-
-    bd = BD.BD(path)
+def test_add_value_wrong_value_4(initBDpath):
+    bd = BD.BD(initBDpath)
     with pytest.raises(ValueError):
         bd.add_value({'memory': 62.5, 'cpu': 10.25, 'cpu': 23.2})
     del bd
 
-    con = sqlite3.connect(path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    d = cur.execute("SELECT * FROM data;").fetchall()
+    d = checkBDhasTable(initBDpath)
     assert len(d) == 0
-    con.close()
--- test_main.py
+++ test_main.py
@@ -9,7 +9,9 @@ import sqlite3
 import test_BD
 
 
-def test_main():
+def test_main(tmpdir):
+    tmpdir.join("test_BD")
+    path = str(tmpdir)+"test_BD"
     memsens = SensorMemory.SensorMemory()
     cpusens = CPUSensor.CPUSensor()
     disksens = DiskSensor.DiskSensor()
@@ -20,22 +22,12 @@ def test_main():
     assert 0 <= data["cpu"] <= 100
     assert 0 <= data["disk"] <= 100
 
-    con = sqlite3.connect(test_BD.path)
-    cur = con.cursor()
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    cur.executescript(';'.join(["drop table if exists %s" % i for i in tables]))
-    con.close()
-
-    bd = BD.BD(test_BD.path)
+    bd = BD.BD(path)
     bd.add_value(data)
     del bd
 
-    con = sqlite3.connect(test_BD.path)
-    cur = con.cursor()
-    assert cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='data';").fetchall()[0][0] == 1
-    d = cur.execute("SELECT * FROM data;").fetchall()
+    d = test_BD.checkBDhasTable(path)
     assert len(d) == 1
     assert d[0][0] == data["memory"]
     assert d[0][1] == data["cpu"]
     assert d[0][2] == data["disk"]
-    con.close()
